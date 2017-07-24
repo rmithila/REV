17daa4d97067450f477d842ea02ffb411398ce28
rmithila <mithila.3309@gmail.com>
2017-07-24 15:20:46 +0530

    Create saz.java

---
+++ saz.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class saz
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+String s=scan.next();
+StringBuffer f=new StringBuffer(s);
+f.reverse();
+System.out.print(f);
+}
+}
